c6df0ae94da5030d0ce2909c96b9dcab81f7f1d1
DavidGFolch <davidgfolch@gmail.com>
2017-07-27 12:35:43 +0200

    beauty

--- tools/config/seed.config.ts
+++ tools/config/seed.config.ts
@@ -174,7 +174,7 @@ export class SeedConfig {
    * `index.html`.
    * @type {string}
    */
-  APP_TITLE = 'Welcome to angular-seed!';
+  APP_TITLE = 'Welcome to angular-seeeeed!';
 
   /**
    * Tracking ID.
